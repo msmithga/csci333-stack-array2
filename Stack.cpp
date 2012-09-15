62f37a91c25495bb08b2b9bfd9a339b429831601
Mary Smith-Gagnon <msmithga@unca.edu>
2012-09-15 07:38:11 -0400

    test commit

---
+++ Stack.cpp
@@ -0,0 +1,35 @@
+#include "Stack.h"
+#include <iostream>
+
+Stack::Stack(int initialSize) {
+  theStack = new int[initialSize];
+  top = 0;
+}
+
+Stack::~Stack() {
+  delete[] theStack;
+}
+
+void Stack::push(int value) {
+  //adding a comment to Stack.cpp
+  //if theStack is full
+  // create new stack twice as big
+  // copy all elements to new stack
+  // delete old stack
+  // point old stack pointer to new stack
+  theStack[top] = value;
+  top++;
+}
+
+int Stack::pop() {
+  top--;
+  return theStack[top];
+}
+
+int Stack::peek() {
+  return theStack[top-1];
+}
+
+int Stack::size() {
+  return top;
+}
